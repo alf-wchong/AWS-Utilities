33d7058b1a199617a9f9a63d80c3e5f8ad9dc85d
chongwmAmz <118857753+chongwmAmz@users.noreply.github.com>
2024-02-14 17:07:08 -0500

    Update PullPush.java
    Input file listing image URIs to pull from

--- Java/ECR/PullPush.java
+++ Java/ECR/PullPush.java
@@ -2,10 +2,12 @@ package org.chongwm;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class PullPush
@@ -33,6 +35,19 @@ public class PullPush
 		{	return imageName+":"+imageTag;		}
 		
 	}
+	
+	protected String imageListFilePath =null;
+	protected String awsRegion =null;
+	protected String logFilePath =null;
+
+	public PullPush(String imageListFilePath, String awsRegion, String logFilePath)
+	{
+		this.imageListFilePath = imageListFilePath;
+		this.awsRegion = awsRegion;
+        this.logFilePath =logFilePath;
+        runShell("echo '**************** PullPush with "+imageListFilePath+". Started **********' >> "+logFilePath);
+        runShell("date --iso-8601=seconds -u | TZ='America/New_York' date +\"%Y-%m-%dT%H:%M:%S%:z\" >> "+logFilePath);
+	}
 
 	private List<DockerImage>  StringToImageArray(String str)
 	{
@@ -52,7 +67,7 @@ public class PullPush
 		if ("0".compareTo(output.substring(0,output.indexOf('\n')))==0)
 		{
 			String repoUri = output.substring(output.indexOf('\n')+1, output.length());
-			//"repositoryUri": "XXXXXXXX.dkr.ecr.us-east-2.amazonaws.com/<originalImageUri>"
+			//"repositoryUri": "xxxxxxxxxx.dkr.ecr.us-east-2.amazonaws.com/quay.io/alfresco/alfresco-elasticsearch-live-indexing-metadata"
 			//String region = output.split(".")[3];
 					
 			String repoPrefix = repoUri.substring(0,repoUri.indexOf("/"));
@@ -64,13 +79,31 @@ public class PullPush
 		return null;
 	}
 	
-	private void PushToECR(List<DockerImage> localImages, String region)
+	protected int PullImagesAllTags(String imageUrl)
 	{
 		String cmdString = null;
+		
+		System.out.println("Starting pull of all tags of "+ imageUrl +".");
+		cmdString = "docker pull "+ imageUrl+" -a";
+		String result = runShell(cmdString);
+		Log(imageUrl+" pull returned "+result.charAt(0));
+		return result.charAt(0)-'0';
+	}
+	
+	private void Log (String logStr)
+	{
+        runShell("echo '"+logStr+"' >> "+this.logFilePath);
+	}
+	
+	
+	private void PushToECR(List<DockerImage> localImages)
+	{
+		String cmdString = null;
+		
 		System.out.println("Starting push to ECR. Image counter. Dot=1, colon=10, bang=50, pipe=100. Asterix is a pushFail, cane is a tagFail-pushSkipped.");
 		for (int l=0; l<localImages.size(); l++)
 		{
-			String ecrPrefix =LoginToECR(localImages.get(l).getName(), region);
+			String ecrPrefix =LoginToECR(localImages.get(l).getName(), this.awsRegion);
 			if (ecrPrefix !=null)
 			{   String result;
 			    cmdString = "docker tag "+ localImages.get(l).getImageFQN()+" "+ecrPrefix+"/"+localImages.get(l).getImageFQN();
@@ -82,7 +115,6 @@ public class PullPush
 			    		System.out.print("*");
 			    }
 			    else System.out.print("?");
-			    	
 			}
 			if (l%100>0)
 				if (l%50>0)
@@ -116,7 +148,6 @@ public class PullPush
 			{
 				output.append(line + "\n");
 			}
-
 			int exitVal = process.waitFor();
 			if (exitVal == 0) 
 			{
@@ -136,27 +167,58 @@ public class PullPush
 		return o;
 	}
 	
-	public static void main(String[] args)
+	protected List<String> StringListFromFile() 
 	{
-		//String ecrPrefix = args[1]; //xxxxxxxxxx.dkr.ecr.eu-north-1.amazonaws.com
-		String region = args[0]; //eu-north-1
-		String filter = "grep "+args[1];
+        List<String> lines = new ArrayList<>();
+        
+        try 
+        {
+            File file = new File(this.imageListFilePath);
+            Scanner scanner = new Scanner(file);
+            
+            while (scanner.hasNextLine()) 
+            {
+                String line = scanner.nextLine();
+                lines.add(line);
+            }
+            
+            scanner.close();
+            
+        } catch (FileNotFoundException e) 
+        {
+            System.out.println("Error opening file: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return lines;
+	}
+	
+	public static void main(String[] args)
+	{//Read a list of images from a file specified in args[0] to push into an ECR registry in region specified args[1]. Logfile is args[2]
+		PullPush pp = new PullPush(args[0], args[1], args[2]);
+		List<String> imageUriList = pp.StringListFromFile();
+		for (String imageUri:imageUriList)
+		{				
+			if (pp.PullImagesAllTags(imageUri) == 0)
 		
-		PushPull pp = new PushPull();
 //		String strGetDockerLogin = "aws ecr get-login-password --region "+region+" | docker login --username AWS --password-stdin "+ecrPrefix;
 //		String output = runShell(strGetDockerLogin);
 //		if ("0".compareTo(output.substring(0,output.indexOf('\n')))==0)
-//		{
-			String output = runShell("docker image ls | "+filter);
-			if ("0".compareTo(output.substring(0,output.indexOf('\n')))==0)
 			{
-				output = output.substring(output.indexOf('\n')+1, output.length());
-				List<DockerImage> imageList = pp.StringToImageArray(output);
-				pp.PushToECR(imageList, region);
-				
+				String output = runShell("docker image ls | grep "+imageUri);
+				if ("0".compareTo(output.substring(0,output.indexOf('\n')))==0)
+				{
+					output = output.substring(output.indexOf('\n')+1, output.length());
+					List<DockerImage> imageList = pp.StringToImageArray(output);
+					pp.PushToECR(imageList);
+			        runShell("date --iso-8601=seconds -u | TZ='America/New_York' date +\"%Y-%m-%dT%H:%M:%S%:z\" >> "+pp.logFilePath);
+				}
+			}	
+			else
+			{//no point pushing if the pull is incomplete
+				System.out.println("!*!*!*!*!*!*!*!* "+imageUri+" had pull errors *!*!*!*!*!*!*!*!");
 			}
-		}	
+		}
 
-//	}
+	}
 
 }
